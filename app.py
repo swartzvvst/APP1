3150a4c535987f88a65cae6eaf38381d422fef39
swartzvvst <bryandsviana10@gmail.com>
2025-02-20 14:03:04 -0300

    Corrigindo host e porta no app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def home():
     return "Bem-vindo ao meu aplicativo Flask!"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__":  
+    app.run(host="0.0.0.0", port=10000)  
