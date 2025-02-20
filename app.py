318f4882828bb484f08bb73ec6039c86ff277bd5
swartzvvst <bryandsviana10@gmail.com>
2025-02-20 14:05:50 -0300

    Corrigindo porta do Flask para Render

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def home():
     return "Bem-vindo ao meu aplicativo Flask!"
 
-if __name__ == "__main__":  
-    app.run(host="0.0.0.0", port=10000)  
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Pega a porta do ambiente ou usa 5000
+    app.run(host="0.0.0.0", port=port)
